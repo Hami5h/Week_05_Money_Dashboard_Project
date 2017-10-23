beebd2feae5807f6e9c741a54ec9c57ce5dc4acf
Hamish <hamishfpso@gmail.com>
2017-10-23 13:27:17 +0100

    Transaction Total working

--- models/transaction.rb
+++ models/transaction.rb
@@ -12,21 +12,36 @@ class Transaction
   end
 
   def save()
-  sql = "INSERT INTO transactions
-  (amount, merchant_id, tag_id)
-  VALUES
-  ($1, $2, $3)
-  RETURNING id"
-  values = [@amount, @merchant_id, @tag_id]
-  results = SqlRunner.run(sql, values)
-  @id = results.first()['id'].to_i
-end
+    sql = "INSERT INTO transactions
+    (amount, merchant_id, tag_id)
+    VALUES
+    ($1, $2, $3)
+    RETURNING id"
+    values = [@amount, @merchant_id, @tag_id]
+    results = SqlRunner.run(sql, values)
+    @id = results.first()['id'].to_i
+  end
 
-def self.all()
+  def self.all()
     sql = "SELECT * FROM transactions"
     values = []
     results = SqlRunner.run( sql, values )
     return results.map { |transaction| Transaction.new(transaction) }
+  end
+
+  def self.find(id)
+    sql = "SELECT * FROM transactions
+    WHERE id = $1"
+    values = [id]
+    results = SqlRunner.run(sql, values)
+    return Transaction.new(results.first)
+  end
+
+  def self.total()
+    sql = "SELECT SUM(amount) as sum FROM transactions"
+    values = []
+    results = SqlRunner.run(sql, values)
+    return results.first["sum"].to_i
   end
 
 end
