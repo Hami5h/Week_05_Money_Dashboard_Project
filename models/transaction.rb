3e00cd6a1d24a62792ae3ea9b46898a059852be0
Hamish <hamishfpso@gmail.com>
2017-10-23 09:47:39 +0100

    models created' '

---
+++ models/transaction.rb
@@ -0,0 +1,15 @@
+require_relative( '../db/sql_runner' )
+
+class Transacation
+
+  attr_reader( :name, :amount, :merchant_id, :tag_id, :id )
+
+  def initialize( options )
+    @id = options['id'].to_i if options['id']
+    @name = options['name']
+    @amount = options['amount'].to_i
+    @merchant_id = options['merchant_id'].to_i
+    @tag_id = options['tag-id'].to_i
+  end
+
+end
